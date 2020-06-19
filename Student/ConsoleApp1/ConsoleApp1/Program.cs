7ec307eac4ad48b2024ab5ce31f6b463a61a0c07
Savely-ISiP-22 <66845696+Savely-ISiP-22@users.noreply.github.com>
2020-06-19 12:27:42 +0500

    Add files via upload

---
+++ Student/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+            static void Main(string[] args)
+            {
+                string[] file;
+                string[] file1;
+                int student = 0;
+                file = File.ReadAllLines("text.txt");
+                for (int i = 0; i < file.Length; i++)
+                {
+                    file1 = file[i].Split(' ');
+                    for (int j = 0; j < file1.Length; j++)
+                        if (file1[j].Contains("STUDENT")) student++;
+
+                }
+                Console.WriteLine("STUDENT встречается " + student + " раз(а)");
+                Console.ReadLine();
+            }
+        }
+    }
+
+
